e8d681037d82224787f42096a0a90d3b049f5a8f
LastCow9000 <sys19912002@hanmail.net>
2021-10-21 03:44:47 +0900

    [feat] update todo api

--- Todo_node/controllers/tasks.js
+++ Todo_node/controllers/tasks.js
@@ -32,10 +32,6 @@ const getTask = async (req, res) => {
   }
 }
 
-const updateTask = (req, res) => {
-  res.send('Update Task')
-}
-
 const deleteTask = async (req, res) => {
   try {
     const { id: taskID } = req.params
@@ -46,6 +42,21 @@ const deleteTask = async (req, res) => {
     res.status(200).json({ task })
     // res.status(200).send()
     // res.status(200).json({ task: null, status: 'success' })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true, runValidators: true
+    })
+    if (!task) {
+      res.status(404).json({ msg: `No task with id : ${taskID}` })
+    }
+    res.status(200).json({ task })
   } catch (error) {
     res.status(500).json({ msg: error })
   }
