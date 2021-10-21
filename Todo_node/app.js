2db9800087e31b5aff7dfb6852ef2e894ad35af1
LastCow9000 <sys19912002@hanmail.net>
2021-10-22 03:14:02 +0900

    [feat] 없는 path not found 처리

--- Todo_node/app.js
+++ Todo_node/app.js
@@ -4,11 +4,13 @@ const tasks = require('./routes/tasks');
 const port = 5000;
 const connectDB = require('./db/connect');
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 
 // middleware
-app.use(express.static('./public'))
+app.use(express.static('./public'));
 app.use(express.json());
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
 
 // db connect then app start
 const start = async () => {
