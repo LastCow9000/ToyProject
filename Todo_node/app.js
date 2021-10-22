1cdb36e0b6ff8e0c7acac709ee29505763ac52d3
LastCow9000 <sys19912002@hanmail.net>
2021-10-23 02:59:58 +0900

    [refactor] try/catch, error 반복제거와  middleware 처리

--- Todo_node/app.js
+++ Todo_node/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-const port = 5000;
 const connectDB = require('./db/connect');
 require('dotenv').config()
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
+
 app.use('/api/v1/tasks', tasks);
+
 app.use(notFound);
+app.use(errorHandlerMiddleware)
+
+const port = 5000;
 
 // db connect then app start
 const start = async () => {
