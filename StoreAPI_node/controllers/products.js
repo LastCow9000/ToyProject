4b6f5cf083862d276de9626c2ad4f7363d6a39de
LastCow9000 <sys19912002@hanmail.net>
2021-11-20 17:27:02 +0900

    [feat] 쿼리스트링 받아 paging 기능

--- StoreAPI_node/controllers/products.js
+++ StoreAPI_node/controllers/products.js
@@ -2,7 +2,11 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
 
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(4)
+    .skip(5);
   res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -32,6 +36,12 @@ const getAllProducts = async (req, res) => {
   if (fields) {
     result = result.select(fields.replace(',', ' '))
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit);
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length })
