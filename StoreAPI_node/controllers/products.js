b86fae040014143642dd2e17219ee3520a590241
LastCow9000 <sys19912002@hanmail.net>
2021-11-21 21:30:05 +0900

    [feat] 쿼리스트링으로 가져올 필드의 numeric 필터링 기능

--- StoreAPI_node/controllers/products.js
+++ StoreAPI_node/controllers/products.js
@@ -2,8 +2,8 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
 
-  const products = await Product.find({})
-    .sort('name')
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('price')
     .select('name price')
     .limit(4)
     .skip(5);
@@ -11,7 +11,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {}
 
   if (featured) {
@@ -22,6 +22,29 @@ const getAllProducts = async (req, res) => {
   }
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const regEx = /\b(<|>|>=|=|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach(item => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+
+    });
+
   }
 
   // console.log(queryObject)
@@ -38,7 +61,7 @@ const getAllProducts = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit);
+  const limit = Number(req.query.limit) || 0;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
