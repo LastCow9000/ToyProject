d9a74fd3db2fff688551f618a2443c521c8f9e48
LastCow9000 <sys19912002@hanmail.net>
2021-12-18 21:00:24 +0900

    [add] sign jwt

--- JWT-login/controllers/main.js
+++ JWT-login/controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const CustomeAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
@@ -9,7 +10,14 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomeAPIError('Please provide email and password', 400)
   }
-  res.send('Fake Login/Register/Signup Route')
+
+  // just for demo, normally provided by DB
+  const id = new Date().getDate()
+
+  // try to keep payload small, better experience for user
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
+
+  res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
